ddac3e9962ab2577187297a7ed4a0929103310c8
MahyaMoonWEB <141522064+MahyaMoonWEB@users.noreply.github.com>
2024-07-17 04:05:04 +0600

    login code, hr fxml

---
+++ src/main/java/com/example/descosystem_g22/hrcontroller.java
@@ -0,0 +1,59 @@
+package com.example.descosystem_g22.Moon;
+
+import com.example.descosystem_g22.User;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
+
+public class hrcontroller
+{
+    @javafx.fxml.FXML
+    private Button Recruiment_bttn;
+    @javafx.fxml.FXML
+    private Button payroll_bttn;
+    @javafx.fxml.FXML
+    private TableColumn reasonAttdColumn;
+    @javafx.fxml.FXML
+    private Button employeeAttendance_bttn;
+    @javafx.fxml.FXML
+    private Pane employeeAttendance_pane;
+    @javafx.fxml.FXML
+    private Button performanceEvaluation_bttn;
+    @javafx.fxml.FXML
+    private Button customerComplain_bttn;
+    @javafx.fxml.FXML
+    private TableView attendanceTable;
+    @javafx.fxml.FXML
+    private Button ralTimeReports_bttn;
+    @javafx.fxml.FXML
+    private Button inventory_bttn;
+    @javafx.fxml.FXML
+    private TableColumn absentTableColumn;
+    @javafx.fxml.FXML
+    private Button companyPolicy_bttn;
+    @javafx.fxml.FXML
+    private DatePicker attendanceDatePicker;
+    @javafx.fxml.FXML
+    private TableColumn presentAttdColumn;
+    @javafx.fxml.FXML
+    private TableColumn IDAttdColumn;
+
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void downloadAttendanceOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loadAttdOnClick(ActionEvent actionEvent) {
+    }
+
+
+
+}
